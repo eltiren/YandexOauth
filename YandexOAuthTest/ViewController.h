6b55c432f9113d9bcd1e374814eecc69e1bc496f
Vitaly Evtushenko <eltiren@eltiren.com>
2011-11-02 13:10:37 +0200

    Initial Commit

---
+++ YandexOAuthTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YandexOAuthTest
+//
+//  Created by Vitaly Evtushenko on 11/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
